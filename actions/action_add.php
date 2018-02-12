c7beb052ebe733dfe2985918994ff6d2ceeecf56
Anthony <anthony.reynaud@hetic.net>
2018-02-12 18:19:08 +0100

    files created / db created

---
+++ actions/action_add.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: antho
+ * Date: 12/02/2018
+ * Time: 14:02
+ */
--- index.php
+++ index.php
@@ -1,7 +1,46 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: antho
- * Date: 12/02/2018
- * Time: 10:34
- */
+// CONNECT TO SERVER AND DATABASE
+require_once "init_db.php";
+// GET DATA
+require_once "consult_db.php";
+// START SESSION
+session_start();
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Note</th>
+    </tr>
+
+    <?php
+
+    ?>
+</table>
+
+
+    <form class="add_form" action="actions/ACTION_add.php" method="POST">
+        <input type="text"      name='first_name'   placeholder="First name">
+        <input type="text"      name='last_name'    placeholder="Last name">
+        <input type="text"      name='age'          placeholder="Age">
+        <input type="submit">
+    </form>
+<?php
+// SHOW ERROR MESSAGES
+require_once "show_error_msg.php";
+?>
+</body>
+</html>
