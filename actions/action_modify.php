1f5f5223052450d9bb448ea1afaae0cc57b5ffe8
Marie Te <marie.te@hetic.net>
2018-02-13 15:07:05 +0100

    add update

--- actions/action_modify.php
+++ actions/action_modify.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: antho
- * Date: 12/02/2018
- * Time: 14:03
- */
+require_once "../init_db.php";
+
+$sql = "UPDATE
+      `anime`
+SET
+   `name` = :name,
+   `genre` = :genre;
+   `picture` = :picture;
+   `date` = :date;
+WHERE
+   `id` = :id
+;";
+
+$stmt = $conn->prepare($sql);
+$stmt->bindValue(':id', $_POST['id']);
+$stmt->bindValue(':name', $_POST['name']);
+$stmt->bindValue(':genre', $_POST['genre']);
+$stmt->bindValue(':picture', $_POST['picture']);
+$stmt->bindValue(':date', $_POST['date']);
+$stmt->execute();
+header('Location: ../index.php?id='.$conn->lastInsertId());
