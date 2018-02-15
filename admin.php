598cbdf442d27af4b3ce15862da1c6ca11398d35
M00NBOY <zeroualmahel@gmail.com>
2018-02-15 09:54:32 +0100

    changement

--- admin.php
+++ admin.php
@@ -14,8 +14,8 @@ session_start();
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style_admin.css">
     <title>Document</title>
 </head>
 
