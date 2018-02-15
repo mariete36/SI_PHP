e8dcaec4d50f8f02aa0748b100cba9f85ee1fdbb
Anthony <anthony.reynaud@hetic.net>
2018-02-15 13:27:19 +0100

    admin panel stylized

--- admin.php
+++ admin.php
@@ -1,7 +1,7 @@
 <?php
 // CONNECT TO SERVER AND DATABASE AND GET DATA
 require_once "init_db.php";
-require 'is_admin.php';
+//require 'is_admin.php';
 ?>
 
 <!doctype html>
@@ -11,106 +11,114 @@ require 'is_admin.php';
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style_admin.css">
     <title>Document</title>
 </head>
 
-<body>
-<a href="index.php">index</a>
-<div class="read">
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>name</th>
-            <th>genre</th>
-            <th>picture</th>
-            <th>year</th>
-        </tr>
-
-        <?php
-        /* on utilise SELECT pour selectionner les noms des données et FROM pour indiquer depuis quelle table.*/
-        $sql = "SELECT
-            `id`,
-            `name`,
-            `genre`,
-            `url`,
-            `year`
-          FROM
-            `anime`
-        ;";
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-        ?>
-        <?php
-        /* parcourt le tableau donné par fetch et affiche un tr tant qu'il trouve une ligne dans la table. */
-        while (false !== $row = $stmt->fetch(PDO::FETCH_ASSOC)) :?>
-            <tr>
-                <td><?=$row["id"]?></td>
-                <td><a href="anime_description.php?id=<?=$row["id"]?>"><?=$row["name"]?></a></td>
-                <td><?=$row["genre"]?></td>
-                <td><?=$row["url"]?></td>
-                <td><?=$row["year"]?></td>
-                <td class="modifyLink">
-                    <form action="update.php" method="POST">
-                        <input type="hidden" name="id" value="<?=$row["id"]?>">
-                        <input type="submit" value="Modify">
-                    </form>
-                </td>
-                <td class="deleteButton">
-                    <form action="actions/action_delete.php" method="POST">
-                        <input type="hidden" name="id" value="<?=$row["id"]?>">
-                        <input type="submit" value="Delete">
-                    </form>
-                </td>
-            </tr>
-        <?php endwhile;?>
-
-    </table>
-</div>
-
-<a href="add.php">Add</a>
+<body class="body">
+    <div class="container">
+        <a class="index" href="index.php"><img src="image/retourindex.png" alt=""></a>
 
 
-    <div class="read">
-        <table>
-            <tr>
-                <th>ID</th>
-                <th>username</th>
-                <th>password</th>
-            </tr>
+        <header class="header">
+            <img src="image/adminlogo.png" alt="logo">
+        </header>
 
-            <?php
-            /* on utilise SELECT pour selectionner les noms des données et FROM pour indiquer depuis quelle table.*/
-            $sql_user = "SELECT
-                    `id`,
-                    `username`,
-                    `password`
-                  FROM
-                    `user`
-                ;";
-            $stmt_user = $conn->prepare($sql_user);
-            $stmt_user->execute();
-            ?>
-
-            <?php
-            /**
-             * parcours le tableau donné par fetch et affiche un tr tant qu'il trouve une ligne dans la table.
-             * */
-            while (false !== $row = $stmt_user->fetch(PDO::FETCH_ASSOC)) :?>
+        <div class="read">
+            <table class="table">
                 <tr>
-                    <td><?=$row["id"]?></td>
-                    <td><?=$row["username"]?></td>
-                    <td><?=$row["password"]?></td>
-                    <td>
-                        <form action="actions/action_user_delete.php" method="POST">
-                            <input type="hidden" name="id" value="<?=$row["id"]?>">
-                            <input type="submit" value="delete">
-                        </form>
-                    </td>
+                    <th class="th">ID</th>
+                    <th class="th">Name</th>
+                    <th class="th">Genre</th>
+                    <th class="th">Extension</th>
+                    <th class="th">Year</th>
+                    <th class="th">Modify/</br>Delete</th>
                 </tr>
-            <?php endwhile;?>
-        </table>
+
+                <?php
+                /* on utilise SELECT pour selectionner les noms des données et FROM pour indiquer depuis quelle table.*/
+                $sql = "SELECT
+                    `id`,
+                    `name`,
+                    `genre`,
+                    `url`,
+                    `year`
+                  FROM
+                    `anime`
+                ;";
+                $stmt = $conn->prepare($sql);
+                $stmt->execute();
+                ?>
+                <?php
+                /* parcourt le tableau donné par fetch et affiche un tr tant qu'il trouve une ligne dans la table. */
+                while (false !== $row = $stmt->fetch(PDO::FETCH_ASSOC)) :?>
+                    <tr>
+                        <td class="td" ><?=$row["id"]?></td>
+                        <td class="td" ><a href="anime_description.php?id=<?=$row["id"]?>"><?=$row["name"]?></a></td>
+                        <td class="td" ><?=$row["genre"]?></td>
+                        <td class="td" ><?=$row["url"]?></td>
+                        <td class="td" ><?=$row["year"]?></td>
+                        <td class="modifyLink deleteButton td">
+                            <form action="update.php" method="POST">
+                                <input type="hidden" name="id" value="<?=$row["id"]?>">
+                                <input class="modifybtn" type="submit" value="Modify">
+                            </form>
+
+                            <form action="actions/action_delete.php" method="POST">
+                                <input type="hidden" name="id" value="<?=$row["id"]?>">
+                                <input class="deletebtn" type="submit" value="Delete">
+                            </form>
+                        </td>
+                    </tr>
+                <?php endwhile;?>
+
+            </table>
+        </div>
+        <div class="flex">
+            <a class="addLink" href="add.php">Add</a>
+        </div>
+            <div class="read marginBottom">
+                <table class="table">
+                    <tr>
+                        <th class="th" >ID</th>
+                        <th class="th" >username</th>
+                        <th class="th" >password</th>
+                        <th class="th" >Delete</th>
+                    </tr>
+
+                    <?php
+                    /* on utilise SELECT pour selectionner les noms des données et FROM pour indiquer depuis quelle table.*/
+                    $sql_user = "SELECT
+                            `id`,
+                            `username`,
+                            `password`
+                          FROM
+                            `user`
+                        ;";
+                    $stmt_user = $conn->prepare($sql_user);
+                    $stmt_user->execute();
+                    ?>
+
+                    <?php
+                    /**
+                     * parcours le tableau donné par fetch et affiche un tr tant qu'il trouve une ligne dans la table.
+                     * */
+                    while (false !== $row = $stmt_user->fetch(PDO::FETCH_ASSOC)) :?>
+                        <tr>
+                            <td class="td" ><?=$row["id"]?></td>
+                            <td class="td" ><?=$row["username"]?></td>
+                            <td class="td" ><?=$row["password"]?></td>
+                            <td class="td" >
+                                <form action="actions/action_user_delete.php" method="POST">
+                                    <input type="hidden" name="id" value="<?=$row["id"]?>">
+                                    <input type="submit" value="delete">
+                                </form>
+                            </td>
+                        </tr>
+                    <?php endwhile;?>
+                </table>
+            </div>
     </div>
 </body>
 </html>
